a47dc03a1813915790c8552794277ede68d54a06
Angus Turner <angusturner27@gmail.com>
2017-01-03 00:40:49 +1000

    updated readme and did some refactoring

--- README.md
+++ README.md
@@ -4,4 +4,14 @@ Implementation of a naive bayes classifier in rust.
 Achieves ~91% accuracy on the wisconsin breast cancer data set:
 http://archive.ics.uci.edu/ml/datasets/Breast+Cancer+Wisconsin+%28Diagnostic%29
 
-More details to come.
+## Project Structure
+
+Naive bayes is implemented entirely in `main.rs`, with `io.rs` and `util.rs`
+providing a handful of helper functions for inputting data and matrix
+manipulation.
+
+## Progress
+* [X] Import data from CSV
+* [X] Basic algorithm for discrete input features
+* [ ] Implement Gaussian PDF, to allow for continuous inputs
+* [ ] Refactor, to allow arbitrary number of classes for each feature and a mix of continuous/discrete features
--- src/main.rs
+++ src/main.rs
@@ -23,7 +23,7 @@ fn main() {
 
     // split the data into training and test sets at 0.67 ratio
     let m = data.rows();
-    let split_index = (m as f64 * 0.67).round() as usize;
+    let split_index = (m as f64 * 0.5).round() as usize;
     let (X_train, X_test) = X.split_at(split_index, Axes::Row);
     let (y_train, y_test) = y.split_at(split_index, Axes::Row);
 
@@ -38,81 +38,126 @@ fn main() {
     let J = 10; // number of discrete values that each attribute x_i can take on
     let K = 2; // number of discrete values for the data labels y_i
     let n = X_train.cols(); // number of features
+    let m = X_train.rows(); // number of training examples
     let l = 1f64; // constant for laplace smoothing of the max-likelihood estimates
 
     // get the frequency of each class in the training set
-    let mut freq: Vec<f64> = vec![0f64; K];
-    freq = y_train.row_iter()
-    .fold(freq, |mut acc, y_i| {
-        acc[y_i[0] as usize] += 1.0;
-        acc
-    });
+    let freq: Vec<f64> = class_freq(&y_train, K);
 
-    // get the class priors from formula
-    // prior(y_k) = ( #D{Y = y_k} + l ) / ( |D| + l*K ), where
-    // y_k = class we interested in
-    // D = the set of all labels (Y_train)
-    // #D{x} = number of elements in set D fulfilling predicate x
-    // K = number of output classes (malignant / not maligant)
-    // l = smoothing constant (value of 1 for "Laplace Smoothing")
-    let m = y_train.rows();
-    let priors = freq.iter().map(|x| (x + 1.0) / (m as f64 + l * K as f64)).collect::<Vec<f64>>();
-
-    // print out the class priors
-    println!("{:?}", priors);
+    // get the prior probabilities by dividing through by number of training examples
+    let priors: Vec<f64> = class_priors(&freq, l, m);
 
     // sort the training data into groups according to their class label
-    let mut clusters: Vec<Matrix<f64>> = vec![Matrix::zeros(0, n); K];
-    clusters = X_train.row_iter().zip(y_train.row_iter())
-    .fold(clusters, |mut acc, (x_i, y_i)| {
-        acc[y_i[0] as usize] = acc[y_i[0] as usize].vcat(&x_i);
-        acc
-    });
+    let clusters = group_by_class(&X_train, &y_train, K);
 
-    // for every feature-class combination, compute the # occurences of each of the J discrete
-    // values.
-    let mut params: Vec<Matrix<f64>> = vec![Matrix::zeros(J, n); K];
+    // get the likelihoods P (X | Y)
+    let likelihoods: Vec<Matrix<f64>> = likelihoods(&clusters, &freq, J, l);
+
+    // sum the evidence of each test example falling into each class K
+    // NOTE: this is P(Y|X) * P(X). We do not need to divide through by
+    // P(X) in order to make prediction.
+    let evidence: Matrix<f64> = evidence(&X_test, &likelihoods, &priors);
+
+    // calculated predicted classes by taking the column index of the max in each row
+    let prediction: Vec<usize> = row_max(&evidence);
+
+    // calculate test-set accuracy
+    let y_test_vec: Vec<f64> = y_test.iter().map(|x| *x).collect::<Vec<f64>>();
+    let test_acc = accuracy(&prediction, &y_test_vec);
+
+    //
+    println!("Test Accuracy {}", test_acc);
+}
+
+// For each value of the K output classes, construct a matrix that contains the 'likelihood' of
+// of each of the n input features taking on each of its J possible values. Return in a vector.
+fn likelihoods(clusters: &Vec<Matrix<f64>>, freq: &Vec<f64>, J: usize, l: f64) -> Vec<Matrix<f64>> {
+    let n = clusters[0].cols();
+    let K = freq.len();
+    let mut likelihoods: Vec<Matrix<f64>> = vec![Matrix::zeros(J, n); K];
 
     // iterate over each cluster
     for k in 0..clusters.len() {
-        // compute frequency of each value occurence for each feature
+        // compute # occurences of each value for each feature
         for i in 0..clusters[k].rows() {
             // collect row x_i into a vector
             let x_i = clusters[k].row(i).iter().collect::<Vec<&f64>>();
             for j in 0..x_i.len() {
                 let c = *x_i[j] as usize - 1;
-                params[k][[c, j]] += 1.0;
+                likelihoods[k][[c, j]] += 1.0;
             }
         }
 
         // divide through by total occurences of class k, plus the smoothing parameter
-        params[k] = (&params[k] + l) / (freq[k] + (l * J as f64));
+        likelihoods[k] = (&likelihoods[k] + l) / (freq[k] + (l * J as f64));
     }
 
-    // make predictions for the test set
-    let mut probs: Matrix<f64> = Matrix::zeros(X_test.rows(), K);
+    likelihoods
+}
+
+/// Get the priors probabilities for each of the output classes.
+/// prior(y_k) = ( #D{Y = y_k} + l ) / ( |D| + l*K ), where
+/// y_k = the class (i.e malignant / not malignant)
+/// D = the set of all labels (Y_train)
+/// #D{x} = number of elements in set D fulfilling predicate x
+/// K = number of output classes
+/// l = smoothing constant (value of 1 for "Laplace Smoothing")
+fn class_priors(freq: &Vec<f64>, l: f64, m: usize) -> Vec<f64> {
+    let K = freq.len();
+    freq.iter().map(|x| (x + l) / (m as f64 + l * K as f64)).collect::<Vec<f64>>()
+}
+
+/// Calculate the frequency of each label class in the training set
+/// y_train - the training labels
+/// K - the number of labels
+fn class_freq(y_train: &MatrixSlice<f64>, K: usize) -> Vec<f64> {
+    // calculate the frequency of each class occurence
+    let mut freq: Vec<f64> = vec![0f64; K];
+    y_train.row_iter()
+        .fold(freq, |mut acc, y_i| {
+            acc[y_i[0] as usize] += 1.0;
+            acc
+        })
+}
+
+// Group the training examples in X_train by their label, returning a vector of matrices where
+// the i-th entry contains the training examples with label `i`
+fn group_by_class(X_train: &MatrixSlice<f64>,
+                  y_train: &MatrixSlice<f64>,
+                  K: usize)
+                  -> Vec<Matrix<f64>> {
+    let n: usize = X_train.cols();
+    let mut clusters: Vec<Matrix<f64>> = vec![Matrix::zeros(0, n); K];
+    X_train.row_iter()
+        .zip(y_train.row_iter())
+        .fold(clusters, |mut acc, (x_i, y_i)| {
+            acc[y_i[0] as usize] = acc[y_i[0] as usize].vcat(&x_i);
+            acc
+        })
+}
+
+/// Determine the evidence for each test example taking on each of the K possible classes.
+/// P(Y = y_k) * P(X_i | Y = y_k)
+fn evidence(X_test: &MatrixSlice<f64>,
+            likelihoods: &Vec<Matrix<f64>>,
+            priors: &Vec<f64>)
+            -> Matrix<f64> {
+    let K: usize = priors.len();
+    let mut evidence: Matrix<f64> = Matrix::zeros(X_test.rows(), K);
     for k in 0..K {
         for i in 0..X_test.rows() {
             // calculate the probability of class k for this training example
             let x_i = X_test.row(i).iter().collect::<Vec<&f64>>();
-            for j in 0..n {
+            for j in 0..x_i.len() {
                 let c = *x_i[j] as usize - 1;
-                probs[[i, k]] += params[k][[c, j]];
+                evidence[[i, k]] += likelihoods[k][[c, j]];
             }
 
-            // multiply by the class prior
-            probs[[i, k]] = &probs[[i, k]] * priors[k];
+            // multiply by the class prior P(Y = y_k)
+            evidence[[i, k]] = &evidence[[i, k]] * priors[k];
         }
     }
-
-    // convert the probability matrix into a vector containing the index of the maximum in every row
-    let pred: Vec<usize> = row_max(&probs);
-    
-    let y_test_vec: Vec<f64> = y_test.iter().map(|x| *x).collect::<Vec<f64>>();
-    let test_acc = accuracy(&pred, &y_test_vec);
-
-    println!("{:?}", pred);
-    println!("Test Accuracy {}", test_acc);
+    evidence
 }
 
 /// compute the accuracy by mapping the labels and corresponding predictions
@@ -135,6 +180,6 @@ fn map_class_labels(y_i: f64) -> f64 {
     match y_i {
         2.0 => 0.0,
         4.0 => 1.0,
-        _ => panic!("Invalid class label")
+        _ => panic!("Invalid class label"),
     }
 }
